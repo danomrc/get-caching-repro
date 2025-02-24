cf596cbd4d7b87d6f0b77f4687fe295e7775356f
Daniel O'Malley <danielomalley@redcross.org.uk>
2025-02-24 10:14:35 +0000

    init

--- app/api/timestamp/route.ts
+++ app/api/timestamp/route.ts
@@ -2,5 +2,5 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
     // return timestamp
-    return NextResponse.json({ timestamp: Date.now() });
+    return NextResponse.json({ timestamp: Date.now().toLocaleString() });
 }
