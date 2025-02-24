5dc5c21caaa2cbe3faff50f367853a68cdef88b3
Daniel O'Malley <danielomalley@redcross.org.uk>
2025-02-24 10:34:55 +0000

    update what route returns

--- app/api/timestamp/route.ts
+++ app/api/timestamp/route.ts
@@ -1,6 +1,11 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
-    // return timestamp
-    return NextResponse.json({ timestamp: Date.now().toLocaleString() });
+    const searchParams = request.nextUrl.searchParams
+
+    // Convert searchParams to a plain object
+    const queryParams = Object.fromEntries(searchParams.entries())
+    
+    // return timestamp formatted nicely
+    return NextResponse.json({ timestamp: new Date().toISOString(), queryParams });
 }
